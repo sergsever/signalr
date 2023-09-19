0f28a516169bee1a90ae058877aaa41721d4592b
Sergey Lvov <SergeyLS@dmigom.ru>
2023-09-19 12:32:54 +0300

    init

---
+++ ChatHub.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.AspNetCore.SignalR;
+
+namespace signalr
+{
+	public class ChatHub : Hub
+	{
+		public async Task Send(string message)
+		{
+			await this.Clients.All.SendAsync("receive", message);
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+using signalr;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.MapHub<ChatHub>("/chat");
+app.Run();
